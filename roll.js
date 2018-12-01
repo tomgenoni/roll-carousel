695e3095afb8fa322fb52ecdbdbf7c02e6803061
Tom Genoni <tom@atomeye.com>
2018-12-01 11:08:55 -0800

    Fixing keyboard click and focus

--- roll.js
+++ roll.js
@@ -2,10 +2,16 @@ function initRoll() {
     const rolls = document.querySelectorAll(".roll");
 
     rolls.forEach(roll => {
+        // Elements of a roll
+        // -------------------------------------------
+
         const wrap = roll.querySelector(".roll-wrap");
         const arrows = roll.querySelectorAll(".roll-arrow");
         const panes = roll.querySelectorAll(".roll-pane");
         const dots = roll.querySelectorAll(".roll-dot");
+
+        // Capture clicks
+        // -------------------------------------------
 
         arrows.forEach(arrow => {
             arrow.addEventListener("click", arrowTrigger);
@@ -15,20 +21,29 @@ function initRoll() {
             dot.addEventListener("click", dotTrigger);
         });
 
-        document.onkeydown = function(e) {
+        document.onkeyup = function(e) {
             switch (e.keyCode) {
+                case 9:
+                    // On tab focus set the active roll
+                    setActiveRoll();
+                    break;
                 case 37:
+                    // On left arrow keyboard click, simulate roll arrow mouse click
                     document
                         .querySelector(".roll.is-active .roll-arrow-left")
                         .click();
                     break;
                 case 39:
+                    // On right arrow keyboard click, simulate roll arrow mouse click
                     document
                         .querySelector(".roll.is-active .roll-arrow-right")
                         .click();
                     break;
             }
         };
+
+        // Functions
+        // -------------------------------------------
 
         function animate(step) {
             const value = -(step * 100);
@@ -39,12 +54,16 @@ function initRoll() {
             roll.dataset.step = step;
         }
 
+        // Hide arrow if roll can no longer move in a given direction
+        // Use 'visibility' instead of 'display' so it doesn't lose focus
+        // and move it to the next available element
         function hideArrow(step) {
-            arrows[0].style.display = step == 0 ? "none" : "block";
-            arrows[1].style.display =
-                step == panes.length - 1 ? "none" : "block";
+            arrows[0].style.visibility = step == 0 ? "hidden" : "visible";
+            arrows[1].style.visibility =
+                step == panes.length - 1 ? "hidden" : "visible";
         }
 
+        // Determine which roll is active, sets "is-active" class
         function setActiveDot(step) {
             dots.forEach(({ classList }) => {
                 classList.remove("is-active");
@@ -52,30 +71,39 @@ function initRoll() {
             dots[step].classList.add("is-active");
         }
 
-        function setActiveRoll(step) {
+        // Determine which roll is active, sets "is-active" class
+        function setActiveRoll() {
             rolls.forEach(({ classList }) => {
                 classList.remove("is-active");
             });
-            roll.classList.add("is-active");
+            const focusedEl = document.activeElement;
+            focusedEl.closest(".roll").classList.add("is-active");
         }
 
-        // Click on arrow
+        // Mouse click on arrow
         function arrowTrigger(e) {
             const direction = parseInt(this.dataset.dir);
             const oldStep = parseInt(roll.dataset.step);
             const step = oldStep + -direction;
-            updateRoll(step, this);
+            // Focus not added automatically by Firefox
+            // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#Clicking_and_focus
+            this.focus();
+            updateRoll(step);
         }
 
-        // Click on dot
+        // Mouse click on dot
         function dotTrigger(e) {
             const step = this.dataset.step;
-            updateRoll(step, this);
+            // Focus not added automatically by Firefox
+            // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#Clicking_and_focus
+            this.focus();
+            updateRoll(step);
         }
 
-        function updateRoll(step, roll) {
+        // Updates the active roll
+        function updateRoll(step) {
             if (step < panes.length && step > -1) {
-                setActiveRoll(roll);
+                setActiveRoll();
                 animate(step);
                 setStep(step);
                 setActiveDot(step);
